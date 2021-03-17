ffe46f14709e508fdceef73a870b733d98bf200d
Gabriel Gatzsche <gabriel.gatzsche@inlavigo.com>
2021-03-17 17:58:10 +0100

    PopoverRouterAnimation

--- lib/src/gg_popover_router.dart
+++ lib/src/gg_popover_router.dart
@@ -24,6 +24,9 @@ class GgPopoverRouter extends StatefulWidget {
     required Key key,
     required this.backgroundWidget,
     required this.foregroundRoutes,
+    this.inAnimation,
+    this.outAnimation,
+    this.animationDuration = const Duration(milliseconds: 500),
   }) : super(key: key);
 
   // ...........................................................................
@@ -35,12 +38,22 @@ class GgPopoverRouter extends StatefulWidget {
   /// Only the route belonging to the visible child route is shown.
   final Map<String, Widget Function(BuildContext)> foregroundRoutes;
 
+  /// This animation is applied to the appearing popover
+  final GgAnimationBuilder? inAnimation;
+
+  /// this animation is applied to the disappearing popover
+  final GgAnimationBuilder? outAnimation;
+
+  /// The duration for route transitions.
+  final Duration animationDuration;
+
   @override
   _GgPopoverRouterState createState() => _GgPopoverRouterState();
 }
 
 // #############################################################################
-class _GgPopoverRouterState extends State<GgPopoverRouter> {
+class _GgPopoverRouterState extends State<GgPopoverRouter>
+    with TickerProviderStateMixin {
   // ...........................................................................
   @override
   dispose() {
@@ -52,24 +65,24 @@ class _GgPopoverRouterState extends State<GgPopoverRouter> {
   @override
   void initState() {
     _observeActiveChildChange();
+    _initAnimation();
     super.initState();
   }
 
   // ...........................................................................
   @override
   Widget build(BuildContext context) {
-    if (_nodeToBeShown == null) {
+    if (!_popOverIsVisible) {
       return widget.backgroundWidget;
     } else {
-      return Stack(
+      final child = Stack(
         children: [
           widget.backgroundWidget,
-          GgRouter(
-            widget.foregroundRoutes,
-            key: ValueKey(_nodeToBeShown!.pathHashCode),
-          ),
+          _popOver!,
         ],
       );
+
+      return _animateIn(child);
     }
   }
 
@@ -93,10 +106,44 @@ class _GgPopoverRouterState extends State<GgPopoverRouter> {
   }
 
   // ...........................................................................
+  Widget? _popOver;
+  bool _popOverIsVisible = false;
+
+  // ...........................................................................
   _update() {
     final node = GgRouter.of(context).node;
     setState(() {
       _nodeToBeShown = node.activeChild;
+      _popOverIsVisible = _nodeToBeShown != null;
+
+      if (_popOverIsVisible) {
+        _popOver = GgRouter(
+          widget.foregroundRoutes,
+          key: ValueKey(_nodeToBeShown!.pathHashCode),
+        );
+      }
     });
   }
+
+  // ...........................................................................
+  late AnimationController _animation;
+
+  // ...........................................................................
+  _initAnimation() {
+    _animation =
+        AnimationController(vsync: this, duration: widget.animationDuration);
+  }
+
+  // ...........................................................................
+  _animateIn(Widget child) {
+    return child;
+  }
+
+  // ...........................................................................
+  _animateOut(Widget child) {
+    return child;
+  }
+
+  // ...........................................................................
+
 }
